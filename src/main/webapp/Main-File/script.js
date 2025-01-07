eabbf291614e6004152c1e1ad12faaf6623bf263
Abhay Kumar Yadav <148680890+abhayy-007@users.noreply.github.com>
2025-01-07 23:47:24 +0530

    Delete src/main directory

--- src/main/webapp/Main-File/script.js
+++
@@ -1,6 +0,0 @@
-// JavaScript for Rent Now Button Interaction
-document.querySelectorAll(".btn-rent").forEach((button) => {
-  button.addEventListener("click", () => {
-    // alert("You have successfully rented this book!");
-  });
-});
